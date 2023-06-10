531fa571a1439521e840ee86b36f92fcb9e86eca
Yushaku <levanson180200@gmail.com>
2023-06-11 00:46:36 +0700

    add config tailwind and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
---
+++ packages/tailwind/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        grayColor: "#627480",
+        primaryColor: "#FF1CFF",
+        textColor: "#051320",
+        strokeColor: "#F1F2F3",
+        tagColor: "#F6F7F8",
+        blackColor: "#051320",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
