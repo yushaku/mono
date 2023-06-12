60928fb0886c82a6e9a6b55b2f98add92127d5b1
yushaku <levanson180200@gmail.com>
2023-06-12 14:20:18 +0700

    remove unused component

---
+++ apps/story/src/stories/Button.stories.ts
@@ -0,0 +1,37 @@
+import { Button } from "ui";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: "Button",
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: "Button",
+  },
+};
+
+export const Large = {
+  args: {
+    size: "large",
+    label: "Button",
+  },
+};
+
+export const Small = {
+  args: {
+    size: "small",
+    label: "Button",
+  },
+};
---
+++ apps/story/src/stories/Icon.stories.ts
@@ -0,0 +1,17 @@
+import { IconEye } from "ui";
+
+export default {
+  title: "Icons/IconEye",
+  component: IconEye,
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+};
+
+export const Primary = {
+  args: {
+    color: "#ffffff",
+    label: "IconEye",
+  },
+};
