cd79b254d174f9a68cdc5a6df4805ac6c5f2ac60
yushaku <levanson180200@gmail.com>
2023-06-12 13:47:02 +0700

    remove old workshop and add storybook instead

--- apps/story/.storybook/main.js
+++ apps/story/.storybook/main.js
@@ -1,17 +1,19 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-const config: StorybookConfig = {
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/react-webpack5",
     options: {},
   },
   docs: {
     autodocs: "tag",
   },
+  staticDirs: ["../public"],
 };
 export default config;
