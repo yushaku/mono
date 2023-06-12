3d41bddd769c10bf8592bf771491754a30f540c7
Yushaku <levanson180200@gmail.com>
2023-06-12 23:39:30 +0700

    change logo

--- apps/web/app/auth/layout.tsx
+++ apps/web/app/auth/layout.tsx
@@ -1,13 +1,14 @@
-import { BigLogo } from "ui";
 import React from "react";
+import Image from "next/image";
+import space from "@/public/spaceUp.gif";
 
 type Props = { children: React.ReactNode };
 
 const layout = ({ children }: Props) => {
   return (
     <section className="grid h-[100vh] w-[100vw] grid-cols-1 md:grid-cols-2">
-      <div className="flex min-h-[250px] items-center justify-center bg-[#051320]">
-        <BigLogo />
+      <div className="flex min-h-[250px] items-center justify-center bg-[#234f66]">
+        <Image src={space} alt="space up" width={700} height={700} />
       </div>
 
       <div className="flexCenter lg:bg-[#F1F2F3]">{children}</div>
--- packages/tailwind/tailwind.config.js
+++ packages/tailwind/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
