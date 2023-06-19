287c241493f484c458da9d32dee0d58661e6dc60
yushaku <levanson180200@gmail.com>
2023-06-19 17:53:53 +0700

    remove unused

--- apps/apis/src/main.ts
+++ apps/apis/src/main.ts
@@ -1,7 +1,7 @@
+import { AppModule } from './app/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
