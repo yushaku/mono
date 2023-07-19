d266fa739d11775bdfa89b4f0353364a0c889970
yushaku <levanson180200@gmail.com>
2023-07-19 09:31:35 +0700

    add analytics to blog

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Provider } from "@/components/Provider";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Provider>{children}</Provider>
+        <Analytics />
       </body>
     </html>
   );
