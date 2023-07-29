324c9ae7f99721b86c2f8c7a94777a276bbfd155
Yushaku <levanson180200@gmail.com>
2023-07-29 10:18:47 +0700

    fix category page

--- apps/blog/src/app/category/[slug]/loading.tsx
+++ apps/blog/src/app/category/[slug]/loading.tsx
@@ -1,29 +1,10 @@
-import { IntroBlock, CategoryList } from "@/components/IntroBlock";
-import { TopicTitle } from "@/components/TopicTitle";
 import React from "react";
 
 function LoadingPage() {
   return (
-    <section className="grid grid-cols-1 gap-x-10 px-6 md:grid-cols-2 md:px-3 lg:grid-cols-3 lg:p-0">
+    <section className="grid grid-cols-1 gap-x-10 px-6 md:grid-cols-2 md:px-3 lg:grid-cols-3 lg:p-0 animate-pulse">
       <div className="md:col-span-2 animate-pulse">
         <div className="w-[700px] h-[500px] block"></div>
-
-        <TopicTitle title="Latest Posts" className="my-12" />
-
-        <ul className="flex flex-wrap justify-center gap-6 md:flex-nowrap">
-          {[1, 2, 3, 4].map((el) => (
-            <li key={el} className="w-[350px] h-[450px] block"></li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="md:hidden">
-        <TopicTitle title="Relate Topics" className="my-12 text-2xl" />
-      </div>
-
-      <div className="col-span-1 flex flex-wrap md:col-span-2 lg:col-span-1">
-        <IntroBlock />
-        <CategoryList />
       </div>
     </section>
   );
